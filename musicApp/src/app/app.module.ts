d438262e739813a00ed77a87f3213f981945318d
Dave J <davejanovec@gmail.com>
2019-04-24 16:56:52 -0500

    basic components

--- musicApp/src/app/app.module.ts
+++ musicApp/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { SearchMusicComponent } from './search-music/search-music.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { SortPipe } from './pipes/sort.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SignInComponent,
+    SignUpComponent,
+    UserPageComponent,
+    SearchMusicComponent,
+    SortPipe,
+    FilterPipe,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
