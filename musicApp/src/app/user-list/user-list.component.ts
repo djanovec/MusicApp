e682361b3183dc7eaa63553a9978c164437eeb31
ninja <ebecker03@msn.com>
2019-05-09 14:52:23 -0500

    added routerlink back to home with submit on signup page and hid search on home if no string in search box

--- musicApp/src/app/user-list/user-list.component.ts
+++ musicApp/src/app/user-list/user-list.component.ts
@@ -46,7 +46,7 @@ export class UserListComponent implements OnInit {
   delete(id) {
     this.rest.deleteUser(id)
       .subscribe(res => {
-          this.deleteUser();
+          // this.deleteUser();
         }, (err) => {
           console.log(err);
         }
