47c6f7d765092ff53c8c887fcef189241b0c218d
Dave J <davejanovec@gmail.com>
2019-05-10 16:53:54 -0500

    adds commit

---
+++ musicApp/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {RestService} from '../services/rest.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit {
+  submitted = false;
+  public loginUser = {};
+  public value = '';
+  signInForm: FormGroup;
+
+  constructor(private router: Router, private restService: RestService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.loginUser);
+    this.submitted = true;
+    this.restService.userLogin(this.loginUser).subscribe(res => {;
+      if(res["message"]){
+        console.log("this works")
+      }
+      else{
+        console.log("error returned")
+      }
+    })
+
+    }
+    // console.log(this.loginUser);
+
+  
+  // login(): void {
+  //   if (this.email == req.body.email && this.password == req.body.password) {
+  //     this.router.navigate(["user"]);
+  //   } else {
+  //     alert("Invalid credentials");
+  //   }
+  // }
+  }
